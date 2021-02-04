cb1bb42498ed8c4bb1940a886b0c35790a5dbd63
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-04 15:19:12 +0000

    calculator can keep counting resolved

--- calculator/main.py
+++ calculator/main.py
@@ -22,11 +22,13 @@ operator = {
     "/" : divide,
 }
 
+
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
 for oper in operator:
     print(oper)
+
 operation_symbol = input("Pick an operation from the line above: ")
 
 calculate = operator[operation_symbol] # operator[operation_symbol] accesses add.
@@ -35,3 +37,15 @@ answer = calculate(n1, n2)
 
 print(f"{n1} {operation_symbol} {n2} = {answer}")
 
+keep_counting = True
+
+while keep_counting:
+    new_operator_or_stop = input("Enter next operator or type 'stop' to exit ")
+
+    if new_operator_or_stop == "stop":
+        keep_counting = False
+        print("You have exited the calculator")
+    else:
+        n3 = int(input("Enter another number: "))
+        new_answer = operator[new_operator_or_stop](answer, n3)
+        print(new_answer)
