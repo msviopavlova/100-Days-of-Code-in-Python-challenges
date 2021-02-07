5eea38262bdc56ffd88076fba20cdfcfc1e5f5d9
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-07 22:01:54 +0000

    revert back to calculate function

--- blackjack_challenge/main.py
+++ blackjack_challenge/main.py
@@ -24,29 +24,24 @@ score_sum_computer = sum(computer_cards)
 
 def calculate_score(player):
     score = sum(player)
-    return score
 
-
-
-def blackjack(player):
-    current_score = calculate_score(player)
-    if current_score == 21:
+    if score == 21:
         return f"{player} has a Blackjack and wins!"
-    elif current_score > 21:
+    elif score > 21:
         print(player)
         print("game over we are over 21")
     else:
-        another_card = input(f"your score is {current_score} Would you like another card? 'y' or 'n': ")
+        another_card = input(f"your score is {score} Would you like another card? 'y' or 'n': ")
         if another_card == "y":
             deal_card(player)
-            blackjack(player)
+            calculate_score(player)
+
+calculate_score(user_cards)
 
 
+def compare(user, computer):
+    pass
 
-        
-
-
-blackjack(user_cards)
 
 
 
