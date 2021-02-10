9c62793b758acec6cb04c890548070926711e94b
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-10 11:04:56 +0000

    higher lower game completed with scores

--- higherlower/new_higher_lower.py
+++ higherlower/new_higher_lower.py
@@ -1,6 +1,8 @@
 import random
 from datagame import data
 
+
+
 #2 randomo objects
 
 def object_generator():
@@ -8,28 +10,27 @@ def object_generator():
     return random_dict
 
 
-
+score = 0
 def checks(option_A, option_B):
     print(f"Compare A: {option_A['name']}, a {option_A['description']} from {option_A['country']} pssssss following is {option_A['follower_count']}")
     print(f"Against B: {option_B['name']}, a {option_B['description']} from {option_B['country']} pssssss following is {option_B['follower_count']}")
-
+    global score
     answer = input("A or B has more following?: ").casefold()
     if answer == "a":
         if option_A["follower_count"] > option_B["follower_count"]:
+            score+=1
             new_option_B = object_generator()
             checks(option_A, new_option_B)
-
-
         else:
-            print("Game over, you are wrong")
+            print(f"Game over, you are wrong, Your score is {score}")
 
     elif answer == "b":
         if option_B["follower_count"] > option_A["follower_count"]:
+            score+=1
             new_option_A = object_generator()
             checks(option_B, new_option_A)
-
         else:
-            print("Game over, you are wrong")
+            print(f"Game over, you are wrong, Your score is {score}")
 
 
 
