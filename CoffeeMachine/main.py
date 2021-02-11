26a676190d1ed4f1b3ae3f19dc3ad6e5bb1520ec
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-11 11:01:37 +0000

    coffee machine takes money gives change and prints a report of resources

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -30,53 +30,64 @@ resources = {
     "coffee": 100,
 }
 
-print(f"{resources} resources before ")
+#print(f"{resources} resources before ")
 
 def get_change():
+    global make_coffee
     if payment > order_cost:
         change = -(order_cost - payment)
         print(f"Here is your change ${change}")
-        ingredients()
+        ingredients(resources)
     else:
         print("Insufficient amount, money refunded")
+        make_coffee = False
 
 
-def ingredients():
-    global resources
+
+
+def ingredients(resources):
+    global make_coffee
     for ing in resources:
         order_ingredients = order["ingredients"]
-        leftover = resources[ing] - order_ingredients[ing]
-        resources[ing] = leftover
-    return resources
+        if resources[ing] > order_ingredients[ing]:
+            leftover = resources[ing] - order_ingredients[ing]
+            resources[ing] = leftover
+            return resources
+        elif resources[ing] < order_ingredients[ing]:
+            print(f"Sorry, there isn't enough {ing} in the coffee machine")
+            make_coffee = False
 
 
 
-choice = input("What would you like? (espresso/latte/cappuccino): ")
 
-order = MENU[choice]
+make_coffee = True
+while make_coffee:
+    choice = input("What would you like? (espresso/latte/cappuccino)  ☕  : ")
 
-order_cost = order["cost"]
-print(f"Please insert coins. The price is {order_cost}")
+    if choice == "report":
+        print(f"{resources} ")
+    else:
+        order = MENU[choice]
+        order_cost = order["cost"]
+        print(f"Please insert coins 💵. The price is {order_cost}")
 
-quarters_given = int(input("How many quarters? "))
-dimes_given = int(input("How many dimes? "))
-nickles_given = int(input("How many nickles? "))
-pennies_given = int(input("How many pennies? "))
-# TODO add up all money given.
+        quarters_given = int(input("How many quarters? "))
+        dimes_given = int(input("How many dimes? "))
+        nickles_given = int(input("How many nickles? "))
+        pennies_given = int(input("How many pennies? "))
 
-quarter = 0.25
-dime = 0.10
-nickle = 0.05
-penny = 0.01
+        quarter = 0.25
+        dime = 0.10
+        nickle = 0.05
+        penny = 0.01
 
-payment = (quarters_given * quarter)+(dimes_given * dime)+(nickles_given * nickle)+(pennies_given * penny)
-print(f"You have paid: {payment}")
+        payment = (quarters_given * quarter)+(dimes_given * dime)+(nickles_given * nickle)+(pennies_given * penny)
+        print(f"You have paid: {payment}")
+
+        get_change()
 
 
-get_change()
-#TODO check is payment higher than the cost ?
 
-print(f"{resources} resources afterwards")
 
 
 
