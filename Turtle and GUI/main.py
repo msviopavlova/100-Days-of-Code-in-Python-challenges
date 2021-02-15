80834917b319674b156b1f35b1824c199ce9dc4e
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-15 19:23:34 +0000

    color add and thickness of the pen

--- Turtle and GUI/main.py
+++ Turtle and GUI/main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import random
 
 lappi = Turtle()
 lappi.shape("turtle")
@@ -13,19 +13,21 @@ lappi.color("red")
 # start from triangle
 # forward100 each time
 
-
+random_color = ["forest green", "red", "cyan", "deep pink", "magenta", "purple", "gold", "orchid", "yellow", "blue"]
 corner = 3
 keep_drawing = True
 while keep_drawing:
-
+    lappi.pensize(5)
     for _ in range(corner): # because 6 shapes
         degrees = 360/corner
         lappi.right(degrees)
         lappi.forward(100)
     corner+=1
+    lappi.pencolor(random.choice(random_color))
 
     if corner >=9:
          keep_drawing = False
+
 
 
 screen = Screen()
