70b633749cb630cee53596f3e42b4a1d31e1e5ff
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-15 15:16:19 +0000

    quiz brain add

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
@@ -8,7 +9,9 @@ for item in question_data:
     question = Question(q_text, q_answer)
     question_bank.append(question)
 
-print(question_bank)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
 
 
---
+++ quiz-game-start/quiz_brain.py
@@ -0,0 +1,10 @@
+class QuizBrain:
+
+    def __init__(self, questions_list):
+        self.question_number = 0
+        self.q_list = questions_list
+
+
+    def next_question(self):
+        current_question = self.q_list[self.question_number]
+        input(f"Q.{self.question_number}: {current_question.text} (True/False)")
