a414ef4c8598912a07c77e0b829dead859a25add
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-18 22:07:43 +0000

    testcase with only negative numbers passed

--- pythonProject/temperatures.py
+++ pythonProject/temperatures.py
@@ -3,23 +3,43 @@ import math
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-zero = 0
+positive_numbers = []
+negative_number = []
 
-list_of_temps = []
-#n = int(input())
+list = []
 
-
-
-# the number of temperatures to analyse
+n = int(input())  # the number of temperatures to analyse
 for i in input().split():
     # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
-    list_of_temps.append(t)
-#lowest_temp = min(list_of_temps)
+    list.append(t)
 
+for i in list:
 
+    if i > 0:
+        # print("This number is positive")
+        positive_numbers.append(i)
+    elif i < 0:
+        # print("This number is negative")
+        negative_number.append(i)
+
+if positive_numbers != []:
+    lowest_from_positive = min(positive_numbers)
+    highest_from_negative = max(negative_number)
+
+    # making the negative number a positive one and then comparing them
+
+    new_positive = -highest_from_negative
+
+    if new_positive > lowest_from_positive:
+        print(lowest_from_positive)
+    elif new_positive < lowest_from_positive:
+        print(highest_from_negative)
+else:
+    highest_from_negative = max(negative_number)
+    print(highest_from_negative)
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-print(lowest_temp)
+
--- pythonProject/testing.py
+++ pythonProject/testing.py
@@ -1,29 +1,26 @@
-list = [125, 50, 1, -10, -50]
+list = [-125, -55, -1, -10, -50]
 positive_numbers = []
 negative_number = []
 for i in list:
-
-#    if its over 0 then its positive
-#    find the minim one and return it
-#
-#
-#    if its less than 0 then its negative
-#    find the maximum one and return it
     if i > 0 :
         print("This number is positive")
         positive_numbers.append(i)
     elif i < 0:
         print("This number is negative")
         negative_number.append(i)
+if positive_numbers != []:
 
-lowest_from_positive = min(positive_numbers)
-highest_from_negative = max(negative_number)
+    lowest_from_positive = min(positive_numbers)
+    highest_from_negative = max(negative_number)
 
-# making the negative number a positive one and then comparing them
+    # making the negative number a positive one and then comparing them
 
-new_positive = -highest_from_negative
+    new_positive = -highest_from_negative
 
-if new_positive > lowest_from_positive:
-    print(f"lowest from positive is closer to 0  = {lowest_from_positive} because negative was {highest_from_negative}")
-elif new_positive < lowest_from_positive:
-    print(f"the negative one ({highest_from_negative}) is closer to 0 than {lowest_from_positive}")
+    if new_positive > lowest_from_positive:
+        print(f"lowest from positive is closer to 0  = {lowest_from_positive} because negative was {highest_from_negative}")
+    elif new_positive < lowest_from_positive:
+        print(f"the negative one ({highest_from_negative}) is closer to 0 than {lowest_from_positive}")
+else:
+    highest_from_negative = max(negative_number)
+    print(highest_from_negative)
