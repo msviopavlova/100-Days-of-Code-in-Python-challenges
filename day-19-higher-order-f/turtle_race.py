1f76f0e4225e1341ee746aeaadbfac3ca01ae5a5
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-18 11:31:32 +0000

    race moving

--- day-19-higher-order-f/turtle_race.py
+++ day-19-higher-order-f/turtle_race.py
@@ -1,45 +1,37 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+new_turtle = Turtle()
 screen = Screen()
 
 screen.setup(width=500, height=400)
 
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-100, -50, 0, 50, 100, 150]
+all_turtles = []
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.color("red")
-tim.goto(x=-230, y=-100)
-
-
-lappi = Turtle(shape="turtle")
-lappi.penup()
-lappi.color("purple")
-lappi.goto(x=-230, y=-50)
-
-
-fox = Turtle(shape="turtle")
-fox.penup()
-fox.color("orange")
-fox.goto(x=-230, y=0)
-
-lol = Turtle(shape="turtle")
-lol.penup()
-lol.color("yellow")
-lol.goto(x=-230, y=50)
-
-boobaloo = Turtle(shape="turtle")
-boobaloo.penup()
-boobaloo.color("blue")
-boobaloo.goto(x=-230, y=100)
+if user_bet:
+    game_on = True
+while game_on:
 
 
-jack = Turtle(shape="turtle")
-jack.penup()
-jack.color("green")
-jack.goto(x=-230, y=150)
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+
+
+
+
+
 
 
 
