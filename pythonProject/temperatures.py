ddc8476cb2eb2227d19bb3cfeaa5de089f769749
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-18 22:14:44 +0000

    solved closest to 0 all testcases passed

--- pythonProject/temperatures.py
+++ pythonProject/temperatures.py
@@ -13,31 +13,33 @@ for i in input().split():
     # t: a temperature expressed as an integer ranging from -273 to 5526
     t = int(i)
     list.append(t)
-
-for i in list:
-
-    if i > 0:
-        # print("This number is positive")
-        positive_numbers.append(i)
-    elif i < 0:
-        # print("This number is negative")
-        negative_number.append(i)
-
-if positive_numbers != []:
-    lowest_from_positive = min(positive_numbers)
-    highest_from_negative = max(negative_number)
-
-    # making the negative number a positive one and then comparing them
-
-    new_positive = -highest_from_negative
-
-    if new_positive > lowest_from_positive:
-        print(lowest_from_positive)
-    elif new_positive < lowest_from_positive:
-        print(highest_from_negative)
+if list == []:
+    print(0)
 else:
-    highest_from_negative = max(negative_number)
-    print(highest_from_negative)
+    for i in list:
+        if i > 0:
+            # print("This number is positive")
+            positive_numbers.append(i)
+        elif i < 0:
+            # print("This number is negative")
+            negative_number.append(i)
+
+    if positive_numbers != []:
+        lowest_from_positive = min(positive_numbers)
+        highest_from_negative = max(negative_number)
+
+        # making the negative number a positive one and then comparing them
+
+        new_positive = -highest_from_negative
+        if new_positive == lowest_from_positive:
+            print(lowest_from_positive)
+        elif new_positive > lowest_from_positive:
+            print(lowest_from_positive)
+        elif new_positive < lowest_from_positive:
+            print(highest_from_negative)
+    else:
+        highest_from_negative = max(negative_number)
+        print(highest_from_negative)
 
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
--- pythonProject/testing.py
+++ pythonProject/testing.py
@@ -1,4 +1,4 @@
-list = [-125, -55, -1, -10, -50]
+list = []
 positive_numbers = []
 negative_number = []
 for i in list:
@@ -8,19 +8,22 @@ for i in list:
     elif i < 0:
         print("This number is negative")
         negative_number.append(i)
-if positive_numbers != []:
-
-    lowest_from_positive = min(positive_numbers)
-    highest_from_negative = max(negative_number)
-
-    # making the negative number a positive one and then comparing them
-
-    new_positive = -highest_from_negative
-
-    if new_positive > lowest_from_positive:
-        print(f"lowest from positive is closer to 0  = {lowest_from_positive} because negative was {highest_from_negative}")
-    elif new_positive < lowest_from_positive:
-        print(f"the negative one ({highest_from_negative}) is closer to 0 than {lowest_from_positive}")
+if list==[]:
+    print(0)
 else:
-    highest_from_negative = max(negative_number)
-    print(highest_from_negative)
+    if positive_numbers != []:
+
+        lowest_from_positive = min(positive_numbers)
+        highest_from_negative = max(negative_number)
+
+        # making the negative number a positive one and then comparing them
+
+        new_positive = -highest_from_negative
+
+        if new_positive > lowest_from_positive:
+            print(f"lowest from positive is closer to 0  = {lowest_from_positive} because negative was {highest_from_negative}")
+        elif new_positive < lowest_from_positive:
+            print(f"the negative one ({highest_from_negative}) is closer to 0 than {lowest_from_positive}")
+    else:
+        highest_from_negative = max(negative_number)
+        print(highest_from_negative)
