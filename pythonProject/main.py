c448771707c4ddc63852ca3e78d976399a03205a
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-18 21:05:35 +0000

    challenge solved

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,11 +6,13 @@ import math
 # where you are given inputs (the heights of the mountains)
 # and where you have to print an output (the index of the mountain to fire on)
 # The inputs you are given are automatically updated according to your last actions.
-all_mountains = []
-highest_mountain = 0
+
 
 # game loop
 while True:
+    all_mountains = []
+    highest_mountain = 0
+
     for i in range(8):
         mountain_h = int(input())  # represents the height of one mountain.
         all_mountains.append(mountain_h)
@@ -23,5 +25,5 @@ while True:
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
     # The index of the mountain to fire on.
-
     print(current_index)
+
