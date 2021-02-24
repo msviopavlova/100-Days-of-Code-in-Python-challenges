0d2ed2f910ab15d037ca81d8c4bd261fdf0d7cfa
Violetta Pavlova <msviopavlova@gmail.com>
2021-02-24 14:51:11 +0000

    snake almost functional

---
+++ Snake_project/main.py
@@ -0,0 +1,47 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
--- Snake_project/snake.py
+++ Snake_project/snake.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -18,8 +22,24 @@ class Snake:
             self.segments.append(new_segment)
 
     def move(self):
-        for segment_number in range(len(self.segments)) -1, 0, -1):
+        for segment_number in range(len(self.segments) -1, 0, -1):
             new_x = self.segments[segment_number-1].xcor()
             new_y = self.segments[segment_number -1].ycor()
             self.segments[segment_number].goto(new_x, new_y)
-        self.segments[0]format(20)
+        self.segments[0].forward(20)
+
+    def up(self):
+        if self.segments[0].heading() !=DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() !=UP:
+            self.segments[0].setheading(DOWN)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
