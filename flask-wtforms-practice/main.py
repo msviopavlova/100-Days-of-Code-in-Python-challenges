4e655017d8c2818e47245baac7959f61f75804cb
Violetta Pavlova <msviopavlova@gmail.com>
2021-03-16 17:42:11 +0000

    working code minor impromenets

--- flask-wtforms-practice/main.py
+++ flask-wtforms-practice/main.py
@@ -8,8 +8,10 @@ import os
 
 
 app = Flask(__name__)
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+# SECRET_KEY = os.urandom(32)
+# app.config['SECRET_KEY'] = SECRET_KEY
+app.secret_key = "some secret string"
+
 
 @app.route("/")
 def home():
